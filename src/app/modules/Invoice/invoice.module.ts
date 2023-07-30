836fac39580bf879bbaf83c2d069ff358831493c
Thiago Retamiro <thiago.retamiro@gmail.com>
2023-07-30 14:17:13 -0300

    criando project

--- src/app/modules/Invoice/invoice.module.ts
+++ src/app/modules/Invoice/invoice.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListInvoiceComponent } from './pages/list-invoice/list-invoice.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListInvoiceComponent
+  ],
   imports: [
     CommonModule
   ]
